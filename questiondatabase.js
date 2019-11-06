5efbedc2212a513c77700a127f7247a722218f51
OmarMutd <31300806+OmarMutd@users.noreply.github.com>
2019-11-05 18:42:25 -0600

    Create questiondatabase.js

---
+++ questiondatabase.js
@@ -0,0 +1,60 @@
+const STORE = {
+    questions: [
+      {
+        question: "How many planets are there in the Solar System?",
+        options: [
+          "Six", 
+          "Seven", 
+          "Eight", 
+          "Nine"
+        ],
+        answer: "Eight"
+      },
+  
+      {
+        question: "What is the name of the comet visible from earth every 75-76 years?",
+        options: [
+          "Halley's Comet", 
+          "Clemens's Comet", 
+          "Comet Encke", 
+          "Comet Hyakutake"
+        ],
+        answer: "Halley's Comet"
+      },
+  
+      {
+        question: "What is the name of the eighth planet in our solar system?",
+        options: [
+          "Mars", 
+          "Saturn", 
+          "Uranus", 
+          "Neptune"
+        ],
+        answer: "Neptune"
+      },
+  
+      {
+        question: "How many starts are in our solar system?",
+        options: [
+          "1", 
+          "2", 
+          "100", 
+          "1 million"
+        ],
+        answer: "1"
+      },
+  
+      {
+        question: "What is the name of the solar activity when a star explodes?",
+        options: [
+          "Neubla", 
+          "Solar flare", 
+          "Supernova", 
+          "Blackhole"
+        ],
+        answer: "Supernova"
+      },
+    ],
+   questionAsked: 0,
+   score: 0,
+  };
